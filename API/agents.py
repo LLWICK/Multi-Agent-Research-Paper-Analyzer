4d275b0b026be2b5e016f646ada6265e5e40b50b
LLWICK <177215471+LLWICK@users.noreply.github.com>
2026-03-26 12:14:31 +0530

    backend strucrure

--- API/agents.py
+++
@@ -1,8 +0,0 @@
-#vectorDB
-
-# Vector Data impimentation
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OllamaEmbeddings
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
